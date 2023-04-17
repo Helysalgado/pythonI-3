bdba4c6fde305ae76a1ebace49ff2f19e80330ea
HeladiaSalgado <heladia@ccg.unam.mx>
2023-04-17 10:51:42 -0600

    Update T4_RawToFastA.py

--- tareas/T4_python/scr/T4_RawToFastA.py
+++ tareas/T4_python/scr/T4_RawToFastA.py
@@ -8,32 +8,36 @@ AUTHOR
 DESCRIPTION
         Este programa crea un archivo fasta a partir de la secuencia de DNA en formato .txt
     
-
+        
+USAGE
+    % py T4_RawToFastA.py
+    
 '''
 # ===========================================================================
 # =                            main
 # ===========================================================================
 
 
-#pedrir archivo con la secuendia de DNA al usuario y el nombre de la secuencia de DNA
+#pedir archivo con la secuendia de DNA 
 print("==================================== \n  Ingrese el nombre o la ruta de un archivo tipo .txt con una secuencia de DNA: ")
-archivo_original= input()
+nombre_archivo_entrada = input()
+
+# pedir el nombre de la secuencia de DNA -identificador
 print("\n Ingrese el nombre de la secuencia de DNA:  ")
-nombre_seq= input()
+nombre_seq = input()
 
-#abrir y leer archivo
-archivo_abierto= open(archivo_original) 
-dna_contenido= archivo_abierto.read()
-
-#quitar el caracter de nueva linea
-dna_contenido=dna_contenido.rstrip("\n")
+#abrir y leer archivo de secuencia
+archivo_entrada = open(nombre_archivo_entrada) 
+dna_seq = archivo_entrada.read()
+dna_seq = dna_seq.rstrip("\n")
+archivo_entrada.close()
 
 #crear archivo fasta
 archivo_fasta=open("dna.fasta" , "w")
 
 #escribir el archivo fasta
-archivo_fasta.write(">"+ nombre_seq + "\n" + dna_contenido)
-print("\n\n Se creo el archivo fasta dna.fasta. ")
+archivo_fasta.write(">" + nombre_seq + "\n" + dna_seq)
+print("\n\n Se creo el archivo fasta dna.fasta")
 
 #cerrar archivo
 archivo_fasta.close()
