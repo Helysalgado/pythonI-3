b304d50b4691ccbb56fe72cad5e13f7306fea450
Carlos Michel Mourra Díaz <60996279+MichMourra@users.noreply.github.com>
2023-05-16 11:12:08 +0200

    Update T3_ContenidoAT_GC.py
    Buen código, cumples con los puntos solicitados en la tarea. Mi recomendación seria que siempre cierres los archivos que abres ya sea utilizando .close o la función with open(). 😄

--- tareas/T3_pyhton/scr/T3_ContenidoAT_GC.py
+++ tareas/T3_pyhton/scr/T3_ContenidoAT_GC.py
@@ -21,27 +21,22 @@ print("==================================== \n  Ingrese el nombre o la ruta de u
 archivo_original= input()
 
 #obrir y leer archivo
-archivo_abierto= open(archivo_original) 
-dna_contenido= archivo_abierto.read()
+with open(archivo_original) as archivo_abierto:
+    dna_contenido= archivo_abierto.read()
 
-#quitar el caracter de nueva linea
-dna_contenido=dna_contenido.rstrip("\n")
+    #quitar el caracter de nueva linea
+    dna_contenido=dna_contenido.rstrip("\n")
 
-#calcular porcentaje de AT y GC
-a=dna_contenido.count('A')
-t=dna_contenido.count('T')
-g=dna_contenido.count('G')
-c=dna_contenido.count('C')
+    #calcular porcentaje de AT y GC
+    a=dna_contenido.count('A')
+    t=dna_contenido.count('T')
+    g=dna_contenido.count('G')
+    c=dna_contenido.count('C')
 
-nt_totales= a + t + g + c
+    nt_totales= a + t + g + c
 
-porcentaje_at= ((a+t)/nt_totales)*100
-porcentaje_gc= ((g+c)/nt_totales)*100
+    porcentaje_at= ((a+t)/nt_totales)*100
+    porcentaje_gc= ((g+c)/nt_totales)*100
 
-#imprimir resultados
-print("\n El porcentaje de AT es : ", porcentaje_at, " % \n El porcentaje de GC es: ", porcentaje_gc, " %")
-
-
-
-
-
+    #imprimir resultados
+    print("\n El porcentaje de AT es : ", porcentaje_at, " % \n El porcentaje de GC es: ", porcentaje_gc, " %")
